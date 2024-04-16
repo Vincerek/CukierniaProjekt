1f2558103d243a06821e37d5abaf0a3ef5f90441
KAPIQ03 <kapik0324@gmail.com>
2024-04-16 14:03:35 +0200

    Stworzona baza do dalszej pracy nad projektem

--- CukierniaProjekt/Konto.cs
+++ CukierniaProjekt/Konto.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CukierniaProjekt
 {
-    public partial class Form1 : Form
+    public partial class Konto : Form
     {
-        public Form1()
+        public Konto()
         {
             InitializeComponent();
         }
