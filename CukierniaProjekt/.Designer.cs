d86ac8a53a578d8fd5cb9f5131bfa14b4bbbc060
Filip Jurczuk <fjurczuk@outlook.com>
2024-04-23 20:21:31 +0200

    Zmiana wizualna

--- CukierniaProjekt/.Designer.cs
+++ CukierniaProjekt/.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.panelNaszeCiasta = new System.Windows.Forms.TableLayoutPanel();
+            this.btnDalej = new FontAwesome.Sharp.IconButton();
             this.infOCiescie = new System.Windows.Forms.Label();
             this.btnPrawo = new FontAwesome.Sharp.IconButton();
             this.nazwaCiasta = new System.Windows.Forms.Label();
             this.btnLewo = new FontAwesome.Sharp.IconButton();
             this.zdjCiast = new System.Windows.Forms.Panel();
             this.iconSplitButton1 = new FontAwesome.Sharp.IconSplitButton();
-            this.btnDalej = new FontAwesome.Sharp.IconButton();
             this.panelNaszeCiasta.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,22 +59,42 @@
             this.panelNaszeCiasta.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.panelNaszeCiasta.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.panelNaszeCiasta.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.panelNaszeCiasta.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.panelNaszeCiasta.Size = new System.Drawing.Size(896, 341);
+            this.panelNaszeCiasta.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.panelNaszeCiasta.Size = new System.Drawing.Size(1195, 420);
             this.panelNaszeCiasta.TabIndex = 0;
+            // 
+            // btnDalej
+            // 
+            this.btnDalej.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(193)))), ((int)(((byte)(238)))));
+            this.btnDalej.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnDalej.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDalej.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDalej.ForeColor = System.Drawing.Color.White;
+            this.btnDalej.IconChar = FontAwesome.Sharp.IconChar.CheckCircle;
+            this.btnDalej.IconColor = System.Drawing.Color.White;
+            this.btnDalej.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnDalej.IconSize = 40;
+            this.btnDalej.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDalej.Location = new System.Drawing.Point(1015, 294);
+            this.btnDalej.Margin = new System.Windows.Forms.Padding(0);
+            this.btnDalej.Name = "btnDalej";
+            this.btnDalej.Size = new System.Drawing.Size(180, 126);
+            this.btnDalej.TabIndex = 6;
+            this.btnDalej.Text = "Dalej";
+            this.btnDalej.UseVisualStyleBackColor = false;
             // 
             // infOCiescie
             // 
             this.infOCiescie.AutoSize = true;
-            this.infOCiescie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(108)))), ((int)(((byte)(152)))));
+            this.infOCiescie.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(193)))), ((int)(((byte)(238)))));
             this.panelNaszeCiasta.SetColumnSpan(this.infOCiescie, 2);
             this.infOCiescie.Dock = System.Windows.Forms.DockStyle.Fill;
             this.infOCiescie.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.infOCiescie.ForeColor = System.Drawing.Color.White;
-            this.infOCiescie.Location = new System.Drawing.Point(0, 238);
+            this.infOCiescie.Location = new System.Drawing.Point(0, 294);
             this.infOCiescie.Margin = new System.Windows.Forms.Padding(0);
             this.infOCiescie.Name = "infOCiescie";
-            this.infOCiescie.Size = new System.Drawing.Size(761, 103);
+            this.infOCiescie.Size = new System.Drawing.Size(1015, 126);
             this.infOCiescie.TabIndex = 4;
             this.infOCiescie.Text = "Informacje o cieście";
             // 
@@ -88,10 +108,10 @@
             this.btnPrawo.IconColor = System.Drawing.Color.White;
             this.btnPrawo.IconFont = FontAwesome.Sharp.IconFont.Solid;
             this.btnPrawo.IconSize = 60;
-            this.btnPrawo.Location = new System.Drawing.Point(761, 68);
+            this.btnPrawo.Location = new System.Drawing.Point(1015, 84);
             this.btnPrawo.Margin = new System.Windows.Forms.Padding(0);
             this.btnPrawo.Name = "btnPrawo";
-            this.btnPrawo.Size = new System.Drawing.Size(135, 170);
+            this.btnPrawo.Size = new System.Drawing.Size(180, 210);
             this.btnPrawo.TabIndex = 2;
             this.btnPrawo.UseVisualStyleBackColor = false;
             this.btnPrawo.Click += new System.EventHandler(this.btnPrawo_Click);
@@ -99,7 +119,7 @@
             // nazwaCiasta
             // 
             this.nazwaCiasta.AutoSize = true;
-            this.nazwaCiasta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(108)))), ((int)(((byte)(152)))));
+            this.nazwaCiasta.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(193)))), ((int)(((byte)(238)))));
             this.panelNaszeCiasta.SetColumnSpan(this.nazwaCiasta, 3);
             this.nazwaCiasta.Dock = System.Windows.Forms.DockStyle.Fill;
             this.nazwaCiasta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
@@ -107,7 +127,7 @@
             this.nazwaCiasta.Location = new System.Drawing.Point(0, 0);
             this.nazwaCiasta.Margin = new System.Windows.Forms.Padding(0);
             this.nazwaCiasta.Name = "nazwaCiasta";
-            this.nazwaCiasta.Size = new System.Drawing.Size(896, 68);
+            this.nazwaCiasta.Size = new System.Drawing.Size(1195, 84);
             this.nazwaCiasta.TabIndex = 0;
             this.nazwaCiasta.Text = "Nazwa Ciasta";
             this.nazwaCiasta.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -122,10 +142,10 @@
             this.btnLewo.IconColor = System.Drawing.Color.White;
             this.btnLewo.IconFont = FontAwesome.Sharp.IconFont.Solid;
             this.btnLewo.IconSize = 60;
-            this.btnLewo.Location = new System.Drawing.Point(0, 68);
+            this.btnLewo.Location = new System.Drawing.Point(0, 84);
             this.btnLewo.Margin = new System.Windows.Forms.Padding(0);
             this.btnLewo.Name = "btnLewo";
-            this.btnLewo.Size = new System.Drawing.Size(134, 170);
+            this.btnLewo.Size = new System.Drawing.Size(179, 210);
             this.btnLewo.TabIndex = 1;
             this.btnLewo.UseVisualStyleBackColor = false;
             this.btnLewo.Click += new System.EventHandler(this.btnLewo_Click);
@@ -133,10 +153,10 @@
             // zdjCiast
             // 
             this.zdjCiast.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.zdjCiast.Location = new System.Drawing.Point(134, 68);
+            this.zdjCiast.Location = new System.Drawing.Point(179, 84);
             this.zdjCiast.Margin = new System.Windows.Forms.Padding(0);
             this.zdjCiast.Name = "zdjCiast";
-            this.zdjCiast.Size = new System.Drawing.Size(627, 170);
+            this.zdjCiast.Size = new System.Drawing.Size(836, 210);
             this.zdjCiast.TabIndex = 3;
             // 
             // iconSplitButton1
@@ -151,35 +171,15 @@
             this.iconSplitButton1.Size = new System.Drawing.Size(23, 23);
             this.iconSplitButton1.Text = "iconSplitButton1";
             // 
-            // btnDalej
-            // 
-            this.btnDalej.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(108)))), ((int)(((byte)(152)))));
-            this.btnDalej.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnDalej.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDalej.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.btnDalej.ForeColor = System.Drawing.Color.White;
-            this.btnDalej.IconChar = FontAwesome.Sharp.IconChar.CheckCircle;
-            this.btnDalej.IconColor = System.Drawing.Color.White;
-            this.btnDalej.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnDalej.IconSize = 40;
-            this.btnDalej.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDalej.Location = new System.Drawing.Point(761, 238);
-            this.btnDalej.Margin = new System.Windows.Forms.Padding(0);
-            this.btnDalej.Name = "btnDalej";
-            this.btnDalej.Size = new System.Drawing.Size(135, 103);
-            this.btnDalej.TabIndex = 6;
-            this.btnDalej.Text = "Dalej";
-            this.btnDalej.UseVisualStyleBackColor = false;
-            // 
             // WybierzTort
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(226)))), ((int)(((byte)(234)))));
-            this.ClientSize = new System.Drawing.Size(896, 341);
+            this.ClientSize = new System.Drawing.Size(1195, 420);
             this.ControlBox = false;
             this.Controls.Add(this.panelNaszeCiasta);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "WybierzTort";
             this.Load += new System.EventHandler(this.WybierzTort_Load);
             this.panelNaszeCiasta.ResumeLayout(false);
