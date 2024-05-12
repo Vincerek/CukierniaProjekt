6c2b6bb565d5a0ea2fcb8f1c04cbe8447d1e91b2
aniao <aniao@LAPTOP-OC2DI8K7>
2024-05-12 18:24:18 +0200

    a

--- CukierniaProjekt/okienkoKoszyk.cs
+++ CukierniaProjekt/okienkoKoszyk.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SQLite;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +18,38 @@ namespace CukierniaProjekt
         {
             InitializeComponent();
         }
+        /*
+        public void baza()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(@"DataSource=..\..\Baza\cukierniaCiasta.db"))
+            {
+                connection.Open();
+                string query = $"SELECT * FROM StworzoneCiasta WHERE Posypka = \"{posypkaTab[indexPosypka]}\" AND Smak = \"{smakTab[indexSmak]}\" AND Baza=\"{bazaTab[indexBaza]}\";";
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            nazwaCiasta.Text = reader["Nazwa Ciasta"].ToString();
+                            infOCiescie.Text = reader["Opis"].ToString();
+
+                            byte[] pic = reader["Zdjecie"] as byte[] ?? null;
+                            if (pic != null)
+                            {
+                                using (MemoryStream mstream = new MemoryStream(pic))
+                                {
+                                    pictures(Image.FromStream(mstream));
+                                }
+                            }
+
+                        }
+                    }
+                }
+                connection.Close();
+            }
+        }*/
 
         private void btnKoszyk_Click(object sender, EventArgs e)
         {
@@ -31,6 +65,9 @@ namespace CukierniaProjekt
             zamowienia.BringToFront();
             zamowienia.Show();
             */
+
+
+
         }
 
         private void btnWroc_Click(object sender, EventArgs e)
